72b06c8d4e986a930232f7929e384a682da194be
Pat Maddox <pat@adorable.io>
2017-03-29 11:32:58 -0500

    Extract some classes

--- migrate_test.rb
+++ migrate_test.rb
@@ -8,42 +8,79 @@ require 'azure/storage'
 require 'aws-sdk'
 require 'benchmark'
 
-$azure_container = 'imagestos3'
-$s3_bucket = 'azure-migration-test'
+module AzureToS3
+  class AzureBlobClient
+    def initialize(container)
+      @container = container
+      @azure_client = Azure::Storage::Client.create
+      @blob_client = @azure_client.blob_client
+    end
 
-# List blobs in container
-azure_client = Azure::Storage::Client.create
-blob_client = azure_client.blob_client
-blob_list = blob_client.list_blobs $azure_container
+    def blob_list
+      @blob_client.list_blobs @container
+    end
 
-# Store blob information in a local database (name, md5, content length)
-local_blobs = []
+    def get_blob_content(blob_name)
+      blob, content = @blob_client.get_blob @container, blob_name
+      content
+    end
 
-blob_list.each do |blob|
-  props = blob.properties
-  local_blobs << {
-    name: blob.name,
-    md5_64: props.fetch(:content_md5),
-    content_length: props.fetch(:content_length)
-  }
+    def fetch_blobs(storage)
+      blob_list.each do |blob|
+        props = blob.properties
+        storage << {
+          name: blob.name,
+          md5_64: props.fetch(:content_md5),
+          content_length: props.fetch(:content_length)
+        }
+      end
+    end
+  end
+
+  class BlobValidator
+    def initialize(client, blob)
+      @client = client
+      @blob = blob
+    end
+
+    def validate
+      content = @client.get_blob_content @blob.fetch(:name)
+      md5 = Digest::MD5.new.tap {|m| m.update(content) }.base64digest
+
+      if md5 == @blob.fetch(:md5_64)
+        @blob[:validated] = :md5
+      elsif content.size == @blob.fetch(:content_length)
+        @blob[:validated] = :length
+      end
+
+      yield(content) if @blob[:validated]
+    end
+  end
+
+  class S3Client
+    def initialize(bucket)
+      @bucket = bucket
+      @s3 = Aws::S3::Client.new
+    end
+
+    def upload_blob(blob, content)
+      @s3.put_object bucket: @bucket, key: blob.fetch(:name), body: content
+    end
+  end
 end
 
-s3 = Aws::S3::Client.new
+local_blobs = []
+blob_client = AzureToS3::AzureBlobClient.new 'imagestos3'
+blob_client.fetch_blobs local_blobs
 
-# Fetch blob content
-# Upload blob to S3, verify md5 and content length
+s3_client = AzureToS3::S3Client.new 'azure-migration-test'
+
 local_blobs.each do |blob|
-  blob_info, content = blob_client.get_blob $azure_container, blob.fetch(:name)
-  md5 = Digest::MD5.new.tap {|m| m.update(content) }.base64digest
-
-  if md5 == blob.fetch(:md5_64)
-    blob[:validated] = :md5
-  elsif content.size == blob.fetch(:content_length)
-    blob[:validated] = :length
+  AzureToS3::BlobValidator.new(blob_client, blob).validate do |content|
+    s3_client.upload_blob blob, content
   end
 
   if blob[:validated]
-    s3.put_object bucket: $s3_bucket, key: blob.fetch(:name), body: content
     puts "Successfully uploaded #{blob.fetch(:name)} (#{blob.fetch(:validated)})"
   else
     $stderr.puts "Blob failed checksum: #{blob.inspect}"
