2ce9f6238ae8a9999fe4519be246df2402186128
Pat Maddox <pat@adorable.io>
2017-03-31 10:20:55 -0500

    Add more stats

--- lib/azure_to_s3/stats_server.rb
+++ lib/azure_to_s3/stats_server.rb
@@ -7,7 +7,14 @@ module AzureToS3
     end
 
     def call(env)
-      [200, {"Content-Type" => "text/plain"}, [@storage.stats.inspect]]
+      [200, {"Content-Type" => "text/plain"}, [stats.inspect]]
+    end
+
+    def stats
+      @storage.stats.merge(
+        uploaders: `ps ax | grep put_to_s3 | grep ruby2.3 | grep -v grep | wc -l`.strip.to_i,
+        fetching: `ps ax | grep fetch_from_azure | grep ruby | grep -v grep | wc -l`.strip.to_i > 0
+      )
     end
   end
 end
