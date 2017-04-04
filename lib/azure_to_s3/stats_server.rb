77d4aab07951f66d13fb20dcaa1df2ae8e8d74a1
Pat Maddox <pat@adorable.io>
2017-04-04 12:47:45 -0600

    No need to search for ruby

--- lib/azure_to_s3/stats_server.rb
+++ lib/azure_to_s3/stats_server.rb
@@ -12,8 +12,8 @@ module AzureToS3
 
     def stats
       @storage.stats.merge(
-        uploaders: `ps ax | grep put_to_s3 | grep ruby2.3 | grep -v grep | wc -l`.strip.to_i,
-        fetching: `ps ax | grep fetch_from_azure | grep ruby | grep -v grep | wc -l`.strip.to_i > 0
+        uploaders: `ps ax | grep put_to_s3 | grep -v grep | wc -l`.strip.to_i,
+        fetching: `ps ax | grep fetch_from_azure | grep -v grep | wc -l`.strip.to_i > 0
       )
     end
   end
