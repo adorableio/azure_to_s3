7f0489966fd0a3fd692ab417fc0e239ac99c509f
Pat Maddox <pat@adorable.io>
2017-04-13 15:52:25 -0600

    Optionally supress output in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
 require_relative '../lib/azure_to_s3'
 
-RSpec.configure do |config|
-  config.before(:all) do
-    $stderr = File.open(File::NULL, "w")
-    $stdout = File.open(File::NULL, "w")
+unless ENV['VERBOSE']
+  RSpec.configure do |config|
+    config.before(:all) do
+      $stderr = File.open(File::NULL, "w")
+      $stdout = File.open(File::NULL, "w")
+    end
   end
 end
 
